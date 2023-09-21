b7262e7bdfab71040739359c9024e93db08c4dd3
ET <realeddietrevino@gmail.com>
2023-09-21 10:18:33 -0500

    fixed intelligent feedback and db routing

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ def get_response():
     
     # Get the current usage count for the user ID from the database
     user = User.query.filter_by(user_id=user_id).first()
+
     try:
         last_user_id_entry = LastUserID.query.first()
         if not user:
@@ -54,7 +55,7 @@ def get_response():
             db.session.commit()
         
         # If the usage count is 3 or more, create a new user ID and reset the usage count
-        elif user.usage_count >= 3:
+        elif user.usage_count > 3:
             # Increment the last used user ID
             last_user_id = last_user_id_entry.last_user_id + 1
             last_user_id_entry.last_user_id = last_user_id
@@ -70,24 +71,6 @@ def get_response():
 
         # Get the learner's response from the POST request body
         user_message = request.json.get('content')           
-
-        # response = openai.ChatCompletion.create(
-        #     model="gpt-3.5-turbo",
-        #     messages=[
-        #         {"role": "system", "content": "The learner is going through a course and answering open-ended questions. You are helping a learner, by providing feedback to those open ended questions. The learner is going through a course were they learn how to respond to a student who has recently made an error, in a way that increases engagement and motivation. The course is structured by presenting scenario 1, then research after scenario 1 on how they(the learner) should respond to students, then the learner is asked open ended questions about that research after scenario 1, finally the learner is shown scenario 2. I will now provide context (in cased in single parantheses ex.'context') the learner is reading as he goes throuh the aformentioned structure in the course. This context will help you provide the feedback that is appropriate to what the learner has just read. In scenario 1 the learner is told, 'You are a tutor to a student, Aaron, who has a long history of struggling with math. Aaron is not particularly motivated to learn math. He just finished a math problem adding a 3-digit and 2-digit number and has made a common mistake (118+18=126, forgot to carry the 1).'. In the research after Scenario 1 the learner is told :'Studies have shown that the way tutors intervene or respond when students make mistakes or show misconceptions in their learning can affect the students motivation to learn. Asking students to try and correct their own mistakes before you help them is a great practice. For this reason, the recommended way of responding to Aaron's mistake would be: \"I appreciate your effort. Lets try solving the problem together. Can you tell me what you did first?\" In addition, it is best to respond indirectly and not to explicitly point out the students mistake by asking leading questions such as, Can you tell me what you did first? Having students find their own mistakes is a powerful approach for three reasons: It helps students recognize easy mistakes on their own, such as making a typo or doing basic math operations incorrectly. Oftentimes, once a student looks at their work again, they will find the obvious error themselves. A students ability to recognize their own errors increases their motivation to learn. It helps students to develop critical thinking skills. The ability to recognize their own errors fosters independent learning skills. It involves students in the learning process and gives students ownership of their learning. It is important to note that you do not want students to get too frustrated. Sometimes students may not have the prior knowledge or ability to find their own mistakes. When a student tries and fails to find their own mistake, coaches should be more explicit by giving the correct answer and support behind it. When responding to students making mistakes or errors it is important to praise the student for putting forth the effort, also called praising for effort. Praising for effort gives students positive emotions even after making a mistake or getting a problem wrong. Praising students for effort and encouraging them to continue trying builds resilience and increases their motivation to learn, In Scenario two the learner reads: ' A student named Jedidiah. He is having trouble solving a math problem. He just finished adding a 3-digit and 2-digit number and has made a common mistake. (213+47=683, didnt line up the number appropriately)'. Now that you have the context to which the leaner has read, you can provide feedback to the learner. To do so every question the learner is asked will begin with \"Question:\" The question will provide enough context for you to understand where they are in the structured course. Then the learner\'s response to the question will always begin with \"The learner response \". The learner\'s response will be the answer to the question that was asked. Finally, you will provide feedback to the learner by responding to the learner\'s response. The feedback you provide will be the answer to the question that was asked, and an example (when applicable) to how the learner should respond where the example starts with a \" quote and ends with a \" quote. Assume your feedback starts with \"Green:\", if the learners response is correct, \"Yellow:\", if the learners response is somewhat correct but missing key ideas, or \"Red:\", if the learners response is incorrect."},
-        #         {"role": "user", "content": "Question:" + question_message},  # Include the question message in the API request
-        #         {"role": "user", "content": f"The learner response:" + user_message}
-        #     ],
-        #     stream=True
-        # )
-        # assistant_message = response.choices[0].message['content']
-
-        # # Save the interaction data in the database
-        # interaction = Interaction(user_id=user_id, question=question_message, learner_response=user_message, llm_response=assistant_message, timestamp=datetime.utcnow())
-        # db.session.add(interaction)
-        # db.session.commit()
-
-        # return jsonify(content=assistant_message)
         def generate(user_message, question_message, user_id):
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
@@ -112,14 +95,6 @@ def get_response():
                     db.session.commit()
                     
                     yield "END"  # Signal the end of the stream
-            
-            # Save the interaction data in the database
-            # interaction = Interaction(user_id=user_id, question=question_message, learner_response=user_message, llm_response=assistant_message, timestamp=datetime.utcnow())
-            # db.session.add(interaction)
-            # db.session.commit()
-
-            # yield "END"  # Signal the end of the stream
-
         
         return Response(stream_with_context(generate(user_message, question_message, user_id)), content_type='text/event-stream')
     
